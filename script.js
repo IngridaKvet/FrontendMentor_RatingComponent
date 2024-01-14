dd55f57cbd503be488e8bd87cbbf72c5e7839e7d
Ingrida <ingridakvetkovskyte@gmail.com>
2024-01-14 21:30:20 +0200

    Basic hmtl and css for both states

---
+++ script.js
@@ -0,0 +1,16 @@
+
+window.onload = function (){
+
+  document.getElementById("ratingContainer").style.display = "flex";
+
+}
+
+//p>You selected</p> <p id="ratingNumber"></p><p>out of 5</p>
+
+
+function submitPressed(){
+  document.getElementById("ratingContainer").style.display = "none";
+  document.getElementById("thanksContainer").style.display = "flex";
+
+
+}
