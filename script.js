20fe35bf9e6dc4e109589dc026ae012acf1d1200
Ingrida <ingridakvetkovskyte@gmail.com>
2024-01-15 19:09:44 +0200

    Functioning rating component

--- script.js
+++ script.js
@@ -8,9 +8,19 @@ window.onload = function (){
 //p>You selected</p> <p id="ratingNumber"></p><p>out of 5</p>
 
 
+let userInput = document.querySelectorAll('.numberBtn');
+
+userInput.forEach(input => input.addEventListener('click', (e) => {
+  let choosenRating = e.target.value;
+
+  console.log(choosenRating)
+
+  document.getElementById("content").innerHTML = "You selected " + choosenRating + " out of 5";
+}
+))
+
+
 function submitPressed(){
   document.getElementById("ratingContainer").style.display = "none";
   document.getElementById("thanksContainer").style.display = "flex";
-
-
 }
